5e15ded96dd0cfdfc88b633de47c7a06341c33b6
skcbroo <skcbroo@gmail.com>
2025-09-19 00:07:32 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -56,7 +56,7 @@ export default function Login() {
         >
           {/* logo opcional no topo do card */}
           <div className="mb-6 text-center select-none cursor-default">
-            <img src="/logonova.png" alt="Midlej Capital" className="mx-auto h-16 mb-3" />
+            <img src="/logo1.png" alt="Midlej Capital" className="mx-auto h-16 mb-3" />
           </div>
 
           <h2
@@ -164,6 +164,7 @@ export default function Login() {
     </NavbarLayout>
   );
 }
+
 
 
 
